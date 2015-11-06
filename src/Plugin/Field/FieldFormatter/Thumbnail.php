a4ce18f30e5a309d58d235054c3851ea090a003f
Sam <sam@1485048.no-reply.drupal.org>
2015-11-06 14:12:19 +0800

    Injection in thumbnail field formatter.

--- src/Plugin/Field/FieldFormatter/Thumbnail.php
+++ src/Plugin/Field/FieldFormatter/Thumbnail.php
@@ -10,6 +10,9 @@ namespace Drupal\video_embed_field\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\video_embed_field\ProviderManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the thumbnail field formatter.
@@ -22,16 +25,22 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class Thumbnail extends FormatterBase {
+class Thumbnail extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The embed provider plugin manager.
+   *
+   * @var \Drupal\video_embed_field\ProviderManagerInterface
+   */
+  protected $providerManager;
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
-    $provider_manager = \Drupal::service('video_embed_field.provider_manager');
     foreach ($items as $delta => $item) {
-      $provider = $provider_manager->loadProviderFromInput($item->value);
+      $provider = $this->providerManager->loadProviderFromInput($item->value);
       $element[$delta] = $provider->renderThumbnail($this->getSetting('image_style'));
     }
     return $element;
@@ -64,6 +73,47 @@ class Thumbnail extends FormatterBase {
   public function settingsSummary() {
     $summary[] = t('Video thumbnail (@quality).', ['@quality' => $this->getSetting('image_style')]);
     return $summary;
+  }
+
+  /**
+   * Constructs a new instance of the plugin.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\video_embed_field\ProviderManagerInterface $provider_manager
+   *   The video embed provider manager.
+   */
+  public function __construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, ProviderManagerInterface $provider_manager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+    $this->providerManager = $provider_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('video_embed_field.provider_manager')
+    );
   }
 
 }
