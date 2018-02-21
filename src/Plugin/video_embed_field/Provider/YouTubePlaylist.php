d1f7d2be2c03909aca4ddb516717331d916c2abe
Seth Fisher <sethfisher@gmail.com>
2018-02-20 22:44:11 -0500

    Added autoplay, start, and index params to YT PL

--- src/Plugin/video_embed_field/Provider/YouTubePlaylist.php
+++ src/Plugin/video_embed_field/Provider/YouTubePlaylist.php
@@ -24,6 +24,9 @@ class YouTubePlaylist extends ProviderPluginBase {
       '#url' => 'https://www.youtube.com/embed/videoseries',
       '#query' => [
         'list' => $this->getVideoId(),
+        'autoplay' => $autoplay,
+        'start' => $this->getTimeIndex(),
+        'index' => $this->getVideoPositionIndex(),
       ],
       '#attributes' => [
         'width' => $width,
@@ -32,6 +35,30 @@ class YouTubePlaylist extends ProviderPluginBase {
         'allowfullscreen' => 'allowfullscreen',
       ],
     ];
+  }
+  
+  /**
+   * Get the time index for when the given playlist video starts.
+   *
+   * @return int
+   *   The time index where the playlist video should start based on the URL.
+   */
+  protected function getTimeIndex() {
+    preg_match('/[&\?]t=(?<timeindex>\d+)/', $this->getInput(), $matches);
+    return isset($matches['timeindex']) ? $matches['timeindex'] : 0;
+  }
+  
+  /**
+   * Get the position index of the video with which the given playlist should
+   * start.
+   *
+   * @return int
+   *   The position index of the video with which the playlist should start
+   *   based on the URL.
+   */
+  protected function getVideoPositionIndex() {
+    preg_match('/[&\?]index=(?<position_index>\d+)/', $this->getInput(), $matches);
+    return isset($matches['position_index']) ? $matches['position_index'] : 0;
   }
 
   /**
